6fdb8c7d88a4821e3708c8c8183466c047b42ce0
Andrei Filip <andreifilip123@gmail.com>
2023-04-09 23:17:55 +0200

    Install react-diff-view

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
+import "react-diff-view/style/index.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
