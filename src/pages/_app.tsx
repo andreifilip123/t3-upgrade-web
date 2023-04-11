b1ad506fb37cb7922a4743fbd19179ebf24038f6
Ethan Coward <ethan@ethancoward.dev>
2023-04-11 18:47:25 +0100

    Use next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Inter } from "next/font/google";
 
 import "@/styles/globals.css";
 import "react-diff-view/style/index.css";
 
+const inter = Inter({ subsets: ["latin"] });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
