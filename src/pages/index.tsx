31db7baa505cfd10db1634ddb959795b0a08357f
PiotrekPKP <peterpkppeter@gmail.com>
2023-04-16 17:47:04 +0200

    feat: default to newest version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,12 +19,12 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 
-const Home: NextPage = () => {
+const UpgradePanel: React.FC<{
+  loading: boolean;
+  versionOptions: VersionsGroupedByMajor;
+}> = ({ loading, versionOptions }) => {
   const router = useRouter();
 
-  const [versionOptions, setVersionOptions] = useState<VersionsGroupedByMajor>(
-    []
-  );
   const [currentVersion, setCurrentVersion] = useState<string | null>(null);
   const [upgradeVersion, setUpgradeVersion] = useState<string | null>(null);
   const [features, setFeatures] = useState<Features>({
@@ -81,15 +81,6 @@ const Home: NextPage = () => {
     return filteredVersions;
   }, [currentVersion, versionOptions]);
 
-  useEffect(() => {
-    const loadT3Versions = async () => {
-      const t3Versions = await getT3VersionsGroupedByMajor();
-      setVersionOptions(t3Versions);
-    };
-
-    void loadT3Versions();
-  }, []);
-
   const renderSelectContent = (options: VersionsGroupedByMajor) => {
     if (!options.length) return null;
 
@@ -123,6 +114,99 @@ const Home: NextPage = () => {
     void router.push(url);
   };
 
+  if (loading) return <span className="text-white">Loding versions...</span>;
+
+  return (
+    <>
+      <div className="flex items-center gap-6">
+        <div>
+          <p className="text-xl text-white">Current version:</p>
+          <Select onValueChange={(value) => setCurrentVersion(value)}>
+            <SelectTrigger className="w-[180px] text-white">
+              <SelectValue placeholder="Select version" />
+            </SelectTrigger>
+            <SelectContent>{renderSelectContent(versionOptions)}</SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          <p className="text-xl text-white">Upgrade to:</p>
+
+          <Select
+            onValueChange={(value) => setUpgradeVersion(value)}
+            disabled={!!noUpgradeAvailable}
+            defaultValue={
+              noUpgradeAvailable
+                ? undefined
+                : upgradeVersionOptions[0]!.versions[0]
+            }
+          >
+            <SelectTrigger className="w-[180px] text-white">
+              <SelectValue
+                placeholder={
+                  noUpgradeAvailable ? "No upgrade available" : "Select version"
+                }
+              />
+            </SelectTrigger>
+            <SelectContent>
+              {renderSelectContent(upgradeVersionOptions)}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-6 text-white">
+        {Object.keys(features).map((feature) => (
+          <div className="flex items-center space-x-4" key={feature}>
+            <Checkbox
+              id={feature}
+              checked={features[feature as keyof typeof features]}
+              onCheckedChange={(value) =>
+                value !== "indeterminate"
+                  ? setFeatures((prev) => ({
+                      ...prev,
+                      [feature]: value,
+                    }))
+                  : null
+              }
+            />
+            <label
+              htmlFor={feature}
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              {feature}
+            </label>
+          </div>
+        ))}
+      </div>
+
+      <button
+        className="rounded-md bg-[hsl(280,100%,70%)] px-4 py-2 text-lg font-medium text-white disabled:cursor-not-allowed disabled:opacity-70"
+        disabled={!currentVersion || !upgradeVersion}
+        onClick={() => goToDiff()}
+      >
+        Upgrade
+      </button>
+    </>
+  );
+};
+
+const Home: NextPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [versionOptions, setVersionOptions] = useState<VersionsGroupedByMajor>(
+    []
+  );
+
+  useEffect(() => {
+    const loadT3Versions = async () => {
+      const t3Versions = await getT3VersionsGroupedByMajor();
+      setVersionOptions(t3Versions);
+      setLoading(false);
+    };
+
+    void loadT3Versions();
+  }, []);
+
   return (
     <>
       <Head>
@@ -135,74 +219,8 @@ const Home: NextPage = () => {
           <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Upgrade <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          <div className="flex items-center gap-6">
-            <div>
-              <p className="text-xl text-white">Current version:</p>
-              <Select onValueChange={(value) => setCurrentVersion(value)}>
-                <SelectTrigger className="w-[180px] text-white">
-                  <SelectValue placeholder="Select version" />
-                </SelectTrigger>
-                <SelectContent>
-                  {renderSelectContent(versionOptions)}
-                </SelectContent>
-              </Select>
-            </div>
 
-            <div>
-              <p className="text-xl text-white">Upgrade to:</p>
-
-              <Select
-                onValueChange={(value) => setUpgradeVersion(value)}
-                disabled={!!noUpgradeAvailable}
-              >
-                <SelectTrigger className="w-[180px] text-white">
-                  <SelectValue
-                    placeholder={
-                      noUpgradeAvailable
-                        ? "No upgrade available"
-                        : "Select version"
-                    }
-                  />
-                </SelectTrigger>
-                <SelectContent>
-                  {renderSelectContent(upgradeVersionOptions)}
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-6 text-white">
-            {Object.keys(features).map((feature) => (
-              <div className="flex items-center space-x-4" key={feature}>
-                <Checkbox
-                  id={feature}
-                  checked={features[feature as keyof typeof features]}
-                  onCheckedChange={(value) =>
-                    value !== "indeterminate"
-                      ? setFeatures((prev) => ({
-                          ...prev,
-                          [feature]: value,
-                        }))
-                      : null
-                  }
-                />
-                <label
-                  htmlFor={feature}
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  {feature}
-                </label>
-              </div>
-            ))}
-          </div>
-
-          <button
-            className="rounded-md bg-[hsl(280,100%,70%)] px-4 py-2 text-lg font-medium text-white disabled:cursor-not-allowed disabled:opacity-70"
-            disabled={!currentVersion || !upgradeVersion}
-            onClick={() => goToDiff()}
-          >
-            Upgrade
-          </button>
+          <UpgradePanel loading={loading} versionOptions={versionOptions} />
         </div>
         <div className="pb-5">
           <a href="https://github.com/andreifilip123/t3-upgrade-web">
