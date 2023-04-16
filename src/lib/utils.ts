e57d4ede741b260276a7fae2acbfc9958de3688e
PiotrekPKP <peterpkppeter@gmail.com>
2023-04-16 18:18:22 +0200

    feat: add syntax highlighting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,16 +5,22 @@ import {
   markEdits,
   HunkData,
 } from "react-diff-view";
+import refractor from "refractor";
+import ts from "refractor/lang/typescript";
 
 export const tokenize = (hunks: HunkData[]) => {
   if (!hunks) {
     return undefined;
   }
 
+  refractor.register(ts);
+
   try {
     return tokennizeReactDiffView(hunks, {
-      highlight: false,
+      highlight: true,
       enhancers: [markEdits(hunks, { type: "block" })],
+      language: "typescript",
+      refractor,
     });
   } catch (e) {
     return undefined;
