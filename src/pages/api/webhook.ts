7faafeeec08cf7938a142a4fd554964c3d1bfe97
PiotrekPKP <peterpkppeter@gmail.com>
2023-04-18 12:48:26 +0200

    feat: add gen logging

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -66,8 +66,10 @@ const handler: NextApiHandler = async (req, res) => {
     (response) => !response.error && !!response.differences && !!response.url
   );
 
-  successfulDiffs.forEach((diff) => {
-    res.revalidate(diff.url || "");
+  successfulDiffs.forEach(async (diff) => {
+    console.log(`Generating page ${diff.url}...`);
+    await res.revalidate(diff.url || "");
+    console.log(`->    Generated ${diff.url}!`);
   });
 
   console.log(
