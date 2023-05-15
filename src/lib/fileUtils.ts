26ae5e2425424a0dc8780e0a4515ca7e9c22c26f
Andrei Filip <andreifilip123@gmail.com>
2023-05-15 08:41:29 +0300

    Fetch diffs from github

--- src/lib/fileUtils.ts
+++ src/lib/fileUtils.ts
@@ -1,4 +1,7 @@
+import { Octokit } from "@octokit/rest";
 import path from "path";
+
+import { env } from "@/env.mjs";
 import { getFeaturesString, type Features } from "./utils";
 
 export interface DiffLocation {
@@ -20,4 +23,38 @@ export const getDiffPath = ({
       featuresString ? `-${featuresString}` : ""
     }.patch`
   );
+};
+
+const octokit = new Octokit({
+  auth: env.GITHUB_PERSONAL_ACCESS_TOKEN,
+});
+
+export const getDiffFromGithub = async (props: DiffLocation) => {
+  const featuresString = getFeaturesString(props.features);
+  const path = `diffs/diff-${props.currentVersion}-${props.upgradeVersion}${
+    featuresString ? `-${featuresString}` : ""
+  }.patch`;
+
+  const { data } = await octokit.repos.getContent({
+    owner: env.GITHUB_DIFFS_OWNER,
+    repo: env.GITHUB_DIFFS_REPO,
+    path,
+  });
+
+  if (Array.isArray(data)) {
+    throw new Error("No file found");
+  }
+
+  const downloadUrl = data.download_url;
+  if (!downloadUrl) {
+    throw new Error("No download url found");
+  }
+
+  const response = await fetch(downloadUrl);
+
+  if (!response.ok) {
+    throw new Error("Failed to download diff");
+  }
+
+  return response.text();
 };
